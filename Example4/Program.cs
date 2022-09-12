ee98f2e4de970bc786cfe72670db779cf1e59059
Stanislav <stasvitalich1994@gmail.com>
2022-09-12 22:20:43 +0700

    Array Sort

--- Example4/Program.cs
+++ Example4/Program.cs
@@ -1,7 +1,9 @@
 ﻿//Упорядочивание данных внутри массива
 
+//Дан массив
 int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
+//Функция, позволяющая распечатать массив
 void PrintArray(int[] array)
 {
     int count = array.Length;
@@ -11,6 +13,29 @@ void PrintArray(int[] array)
     }   
     Console.WriteLine();
 }
-
 PrintArray(arr);
 
+//Функция сортировки массива от большего к меньшему
+void SelectionSort(int[] array)
+{   //Перебираем все значения массива
+    for(int i = 0; i < array.Length - 1; i++)
+    {   //Допустим, что i - минимальное значение и вводим новую переменную
+        int minPosition = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {   //Если конкретное значение меньше минимального, то 
+            //переназначаем минимальное значение
+            if (array[j] < array[minPosition])
+            {
+                minPosition = j;
+            }    
+            //Обмен одной переменной через третью
+            //Замена минимальной позиции с той, которую мы нашли
+            int temporary = array[i];
+            array[i] = array[minPosition];
+            array[minPosition] = temporary;
+        }
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
