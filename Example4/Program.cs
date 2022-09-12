0097a82962dbbcd6fe8f0dfbf54e5f711fa86054
Stanislav <stasvitalich1994@gmail.com>
2022-09-12 21:48:47 +0700

    Print Array

--- Example4/Program.cs
+++ Example4/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Упорядочивание данных внутри массива
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }   
+    Console.WriteLine();
+}
+
+PrintArray(arr);
+
