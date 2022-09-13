68926b5411d2d87a4e6cec32ab96b207858289f5
Stanislav <stasvitalich1994@gmail.com>
2022-09-13 20:50:58 +0700

    Table squares

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход число N
+//и выдаёт таблицу квадратов чисел от 1 до N
+
+//Инициализируем ввод числа
+Console.WriteLine("Введите число: ");
+int initial = int.Parse(Console.ReadLine());
+
+//Выводим все числа от 1 до N при помощи цикла for
+for (int i = 1; i <= initial; i = i + 1)
+{
+    Console.WriteLine(Math.Pow(i, 2));
+}
