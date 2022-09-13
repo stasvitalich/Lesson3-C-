a6d0f567f0db0f27cdc2e2b08850b79f55978312
Stanislav <stasvitalich1994@gmail.com>
2022-09-13 23:28:17 +0700

    TASK23 has been resolved

--- TASK23/Program.cs
+++ TASK23/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход число (N)
+//и выдаёт таблицу кубов чисел от 1 до N.
+//Примеры:
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+//Инициируем ввод числа n
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Таблица кубов от 1 до {n}: ");
+for (int i = 1; i <= n; i = i + 1)
+{   
+    
+    Console.WriteLine(Math.Pow(i, 3));
+}
